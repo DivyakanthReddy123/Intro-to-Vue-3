ccddedcebec8989a72427274b1c3b67340492b2f
DivyakanthReddy123 <44220908+DivyakanthReddy123@users.noreply.github.com>
2022-01-29 13:39:46 +0530

    url added

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg'
+            image: './assets/images/socks_blue.jpg',
+            url: 'https://www.apple.com/in/iphone-13-pro/?afid=p238%7Cs1DxUEtJV-dc_mtid_209254ho67063_pcrid_547839330716_pgrid_127298783516_&cid=wwa-in-kwgo-iphone-slid----Avail-'
         }
     }
 })
