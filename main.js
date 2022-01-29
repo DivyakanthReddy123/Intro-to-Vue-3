1d6b5775b21d0535cd8c791baaac67d7be256b3a
DivyakanthReddy123 <44220908+DivyakanthReddy123@users.noreply.github.com>
2022-01-29 13:05:28 +0530

    new product added

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks',
+            product: 'iPhone 13 Pro Max',
             // Solution
             description: 'A warm fuzzy pair of socks.' 
             // Solution
